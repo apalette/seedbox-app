db679938b04dadd369fb569d6ca344c073357a91
Aurélie Palette <aurelie@codeuz.com>
2017-08-08 17:24:55 +0200

    Home Template : app layout + en language

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+    	<meta charset="utf-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<title>Seedbox</title>
+    	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+		<link href="{{url('css/bootstrap.min.css')}}" rel="stylesheet">
+		<link href="{{url('css/main.css')}}" rel="stylesheet">
+		<!--[if lt IE 9]>
+      		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+	</head>
+  	<body>
+    	<!-- Static navbar -->
+		<nav class="navbar navbar-inverse navbar-static-top">
+      		<div class="container">
+        		<div class="navbar-header">
+          			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            			<span class="sr-only">Menu</span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+          			</button>
+          			<a class="navbar-brand" href="{{url('/')}}">Seedbox</a>
+        		</div>
+        		<div id="navbar" class="navbar-collapse collapse">
+          			<ul class="nav navbar-nav">
+            			<li{!!(url()->current() == url('/')) ? ' class="active"' : ''!!}><a href="{{url('/')}}"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+            			<li{!!(url()->current() == url('/newfile')) ? ' class="active"' : ''!!}><a href="{{url('/newfile')}}"><span class="glyphicon glyphicon-plus-sign"></span> Add File</a></li>
+            			<li><a href="{{url('/users')}}"><span class="glyphicon glyphicon-user"></span> Users</a></li>
+          			</ul>
+          			<ul class="nav navbar-nav navbar-right">
+          				<li><span class="navbar-text">Username <a href="{{url('/logout')}}"><i class="glyphicon glyphicon-log-out"></i></a></span></li>
+          			</ul>
+        		</div>
+      		</div>
+    	</nav>
+    	
+    	<div class="wrapper">
+    		<div class="container">
+    			@yield('content')
+    		</div>
+    	</div>
+
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    	<script src="{{url('js/bootstrap.min.js')}}"></script>
+	</body>
+</html>
