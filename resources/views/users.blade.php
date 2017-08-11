c1cebb36717cf5531439cf72bc823a26c27ef57d
Aurélie Palette <aurelie@codeuz.com>
2017-08-11 17:14:06 +0200

    Users List Template : delete and lock

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -14,35 +14,48 @@
 		        	<th>Created</th>
 		        	<th>Updated</th>
 		        	<th></th>
+		        	<th></th>
 		      	</tr>
 			</thead>
 		    <tbody>
 				<tr>
-					<td><a class="btn btn-primary btn-xs" href="{{url('/users/edit/1')}}"><span class="glyphicon glyphicon-pencil"></span></a></td>
+					<td>
+						<a class="btn btn-primary btn-xs" href="{{url('/users/edit/1')}}"><span class="glyphicon glyphicon-pencil"></span></a>
+						<a class="btn btn-danger btn-xs" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					</td>
 		        	<td>John</td>
 		        	<td>Doe</td>
 		        	<td>john@example.com</td>
 		        	<td>01/21/2016 18:35</td>
 		        	<td>01/21/2016 18:35</td>
-		        	<td><span class="label label-danger">inactive</span></td>
+		        	<td><span class="text-danger">locked</span></td>
+		        	<td><a class="btn btn-success btn-xs" href="#"><span class="glyphicon glyphicon-ok"></span> unlock</a></td>
 		        </tr>
 		      	<tr>
-		      		<td><a class="btn btn-primary btn-xs" href="{{url('/users/edit/1')}}"><span class="glyphicon glyphicon-pencil"></span></a></td>
-		        	<td>Mary</td>
+		      		<td>
+						<a class="btn btn-primary btn-xs" href="{{url('/users/edit/1')}}"><span class="glyphicon glyphicon-pencil"></span></a>
+						<a class="btn btn-danger btn-xs" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					</td>
+		      		<td>Mary</td>
 		        	<td>Moe</td>
 		        	<td>mary@example.com</td>
 		        	<td>01/21/2016 18:35</td>
 		        	<td>01/21/2016 18:35</td>
-		        	<td><span class="label label-warning">pending</span></td>
+		        	<td><span class="text-warning">pending</span></td>
+		        	<td></td>
 		      	</tr>
 		      	<tr>
-		      		<td><a class="btn btn-primary btn-xs" href="{{url('/users/edit/1')}}"><span class="glyphicon glyphicon-pencil"></span></a></td>
-		       		<td>July</td>
+		      		<td>
+						<a class="btn btn-primary btn-xs" href="{{url('/users/edit/1')}}"><span class="glyphicon glyphicon-pencil"></span></a>
+						<a class="btn btn-danger btn-xs" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+					</td>
+		      		<td>July</td>
 		        	<td>Dooley</td>
 		        	<td>july@example.com</td>
 		        	<td>01/21/2016 18:35</td>
 		        	<td>01/21/2016 18:35</td>
-		        	<td><span class="label label-success">active</span></td>
+		        	<td><span class="text-success">active</span></td>
+		        	<td><a class="btn btn-danger btn-xs" href="#"><span class="glyphicon glyphicon-remove"></span> lock</a></td>
 		    	</tr>
 			</tbody>
 		</table>
